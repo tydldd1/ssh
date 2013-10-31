3ed6e5e0770b8352d6659c75cb64e460389e207e
tydldd <ru2013_1@163.com>
2013-10-31 16:00:20 +0800

    Signed-off-by: tydldd <ru2013_1@163.com>

---
+++ ssh0605/src/com/ru/javaExam/io/AudioInputStreamExam.java
@@ -0,0 +1,26 @@
+/**
+ * 文件名：AudioInputStreamExam.java
+ *
+ * 版本信息：
+ * 日期：2013年10月31日
+ * Copyright ru Corporation 2013 
+ * 版权所有
+ *
+ */
+package com.ru.javaExam.io;
+
+/**
+ * 
+ * 项目名称：ssh0605
+ * 类描述：
+ * 创建人：成如
+ * 创建时间：2013年10月31日 下午4:00:09
+ * 修改人：成如
+ * 修改时间：2013年10月31日 下午4:00:09
+ * 修改备注：
+ * @since  jdk1.7
+ * @version 1.0
+ */
+public class AudioInputStreamExam {
+
+}
