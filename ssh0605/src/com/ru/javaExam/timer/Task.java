d0cedc4d938209e88573ce09d65b990a7ec381de
tydldd <ru2013_1@163.com>
2013-11-09 12:01:36 +0800

    Signed-off-by: tydldd <ru2013_1@163.com>

---
+++ ssh0605/src/com/ru/javaExam/timer/Task.java
@@ -0,0 +1,39 @@
+/**
+ * 文件名：Task.java
+ *
+ * 版本信息：
+ * 日期：2013年11月8日
+ * Copyright ru Corporation 2013 
+ * 版权所有
+ *
+ */
+package com.ru.javaExam.timer;
+
+import java.util.TimerTask;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 
+ * 项目名称：ssh0605
+ * 类描述：
+ * 创建人：成如
+ * 创建时间：2013年11月8日 下午6:10:57
+ * 修改人：成如
+ * 修改时间：2013年11月8日 下午6:10:57
+ * 修改备注：
+ * @since  jdk1.7
+ * @version 1.0
+ */
+public class Task extends TimerTask{
+	Logger log = Logger.getLogger(TimerExam.class);
+	private static int i = 0;
+	
+	public void run() {
+		/*log.debug("这个I = " + i++);*/
+		System.out.println(i++);
+	}
+	
+}
+
+
