b34f4099f060e28b14433970b69ef800937bb341
tydldd <ru2013_1@163.com>
2013-11-11 12:54:34 +0800

    Rejected commit: Default

---
+++ ssh0605/src/com/ru/javaExam/nio/CharBufferExam.java
@@ -0,0 +1,20 @@
+package com.ru.javaExam.nio;
+
+import java.nio.CharBuffer;
+
+/**
+ * Description:    新io流字符缓冲区例子
+ * User: NanChengRu
+ * Date: 13-11-11
+ * Time: 上午10:14
+ * JDK: 1.6
+ * version: 1.0
+ */
+public class CharBufferExam {
+    public static void main(String[] args){
+        CharBuffer cb = CharBuffer.allocate(10);
+
+        System.out.println("字符缓冲区的容量：" + cb.capacity());
+        System.out.println("字符缓冲区的限制位置：" + cb.limit());
+    }
+}
